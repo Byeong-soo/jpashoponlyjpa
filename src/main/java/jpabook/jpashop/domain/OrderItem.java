a927bcfeebd015012ef682358977ca347137d655
byeongSoo <ubs4939@naver.com>
2022-05-12 20:53:55 +0900

    상속관계 맵핑

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -7,14 +7,12 @@ import javax.persistence.*;
 
 @Entity
 @Getter @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @Column(name = "ORDER_ID")
-    private Long orderId;
 
     @ManyToOne
     @JoinColumn(name = "ORDER_ID")
